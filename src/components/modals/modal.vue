2d475026691cd821290e27f14ae0d71cd53cde2a
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-06-10 16:51:15 -0500

    feat: add modal component

---
+++ src/components/modals/modal.vue
@@ -0,0 +1,83 @@
+<script lang="ts" setup>
+import { CloseIcon } from "@components/icons";
+import { Transition, onBeforeUnmount, onMounted, ref } from "vue";
+
+const props = defineProps({
+  className: String,
+  show: Boolean,
+});
+
+const emit = defineEmits(["close"]);
+
+const modalContent = ref<HTMLElement | null>(null);
+
+function onOutClick(ev: MouseEvent) {
+  if (!modalContent.value) {
+    throw new Error("Content modal should exist!");
+  }
+  const target = ev.target as HTMLElement;
+
+  if (!modalContent.value.contains(target)) {
+    emit("close");
+  }
+}
+console.log(props);
+
+onMounted(() => {
+  document.addEventListener("click", onOutClick);
+});
+
+onBeforeUnmount(() => {
+  document.removeEventListener("click", onOutClick);
+});
+</script>
+
+<template>
+  <div
+    class="modal-container fixed top-0 bottom-0 left-0 right-0 flex-center z-99"
+    :class="{ 'modal-show': show }"
+  >
+    <Transition name="mdl">
+      <div
+        v-if="show"
+        ref="modalContent"
+        class="modal-content bg-gray-200 relative p-4 pt-8 rounded-lg"
+      >
+        <button
+          @click="$emit('close')"
+          class="bg-gray-300 absolute top-1 right-1 cursor-pointer rounded-0.5"
+        >
+          <CloseIcon class="h-5 w-5 fill-gray-700" />
+        </button>
+        <slot />
+      </div>
+    </Transition>
+  </div>
+</template>
+
+<style scoped>
+div.modal-container {
+  pointer-events: none;
+}
+
+div.modal-show {
+  pointer-events: all;
+}
+
+.mdl-enter-active,
+.mdl-leave-active {
+  transition: opacity 0.2s ease, transform 0.2s ease;
+}
+
+.mdl-enter-from,
+.mdl-leave-to {
+  transform: translateY(-30px);
+  opacity: 0;
+}
+
+div.modal-content {
+  display: grid;
+  overflow: hidden;
+  grid-template-rows: 26px 1fr;
+}
+</style>
