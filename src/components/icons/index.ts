82787b01cc68b17a6d5dd4046a5c206182522152
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-06-15 16:23:36 -0500

    feat: add edit and read icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,3 +2,5 @@ export { default as CardsIcon } from "./cards.vue";
 export { default as BookIcon } from "./book.vue";
 export { default as ArrowIcon } from "./arrow.vue";
 export { default as CloseIcon } from "./close.vue";
+export { default as EditIcon } from "./edit.vue";
+export { default as ReadIcon } from "./read.vue";
