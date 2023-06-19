737e4c0d5062b6e4af6dd2d2d22b7d941fdf2330
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-06-19 17:17:03 -0500

    feat: add chevron icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -6,6 +6,8 @@ export { default as CheckIcon } from "./check.vue";
 export { default as EditIcon } from "./edit.vue";
 export { default as ReadIcon } from "./read.vue";
 
+export { default as ChevronIcon } from "./chevron.vue";
+
 export { default as AlertIcon } from "./alert.vue";
 export { default as InfoIcon } from "./info.vue";
 export { default as ErrorIcon } from "./error.vue";
