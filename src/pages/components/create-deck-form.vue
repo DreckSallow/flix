04b751bf6ed06cc0a6b1f55d19e4528f0ec3d414
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-06-21 22:03:45 -0500

    style: add theme styles to deck form

--- src/pages/components/create-deck-form.vue
+++ src/pages/components/create-deck-form.vue
@@ -81,7 +81,7 @@ async function getFileImport() {
       <div v-if="wayToCreate === 'create'">
         <input
           v-focus
-          class="border border-gray-600/80 border-solid rounded-md p-1"
+          class="border border-strong border-solid rounded-md p-1"
           type="text"
           tabindex="1"
           v-model="newDeckName"
@@ -97,7 +97,7 @@ async function getFileImport() {
 
       <div class="flex flex-row gap-4 justify-end mt-4">
         <button
-          class="bg-blue-400 text-white px-2 py-1 text-xs rounded-1 cursor-pointer"
+          class="bg-strong text-white px-2 py-1 text-xs rounded-1 cursor-pointer"
           tabindex="2"
           @click="createDeck"
         >
@@ -105,7 +105,7 @@ async function getFileImport() {
         </button>
         <button
           tabindex="3"
-          class="bg-blue-400 text-white px-2 py-1 text-xs rounded-1 cursor-pointer"
+          class="bg-strong text-white px-2 py-1 text-xs rounded-1 cursor-pointer"
           @click="$emit('cancel')"
         >
           Cancel
@@ -123,6 +123,6 @@ nav li {
   padding-bottom: 2px;
 }
 nav li.selected {
-  border-color: rgb(96, 165, 250);
+  border-color: hsla(var(--bg-strong), 1) !important;
 }
 </style>
