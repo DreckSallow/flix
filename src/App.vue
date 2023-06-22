9fcfd493f62fd69fa0d196d792053e9b69cbf795
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-06-22 11:06:50 -0500

    fix: fix remove notification in TransitionGroup

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,56 @@
 <script lang="ts" setup>
-import { computed } from "vue";
 import { NotifyState } from "./state";
 import Notification from "@components/notification.vue";
 
 function closeNotify(k: number) {
   NotifyState.remove(k);
 }
-const displayNotify = computed(() => NotifyState.notifications.length > 0);
 </script>
 
 <template>
   <router-view></router-view>
-  <article
-    class="fixed bottom-2 right-2 w-300px z-999 overflow-auto"
-    v-if="displayNotify"
-  >
-    <ul class="p-3 flex flex-col gap-4">
+  <article class="fixed bottom-2 right-2 w-300px z-999 overflow-auto">
+    <TransitionGroup
+      class="p-3 flex flex-col gap-4"
+      name="notifications"
+      tag="ul"
+    >
       <li
         v-for="(notification, i) in NotifyState.notifications"
-        class="overflow-auto bg-gray-200 rounded-md"
+        class="overflow-auto bg-accent rounded-md"
+        :key="notification.id"
       >
         <Notification
           @close="closeNotify"
           :type="notification.type"
-          :keyId="i"
-          :text="notification.content"
+          :keyId="notification.id"
+          :content="notification.content"
+          :debug="notification.debug"
           :title="notification.title"
         />
       </li>
-    </ul>
+    </TransitionGroup>
   </article>
 </template>
 
 <style scoped>
-/* article ul{
-  padding: ;
-} */
-
 article {
   max-height: 240px;
+  overflow-x: hidden;
 }
 
 article li {
   max-height: 100px;
+  min-height: 40px;
+}
+
+.notifications-enter-active,
+.notifications-leave-active {
+  transition: all 0.5s ease;
+}
+.notifications-enter-from,
+.notifications-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
