65140e83f858c4f1214268329efb96105cd7bc60
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-06-28 15:54:24 -0500

    fix: fix the deck card style

--- src/pages/home/deck-section.vue
+++ src/pages/home/deck-section.vue
@@ -55,22 +55,19 @@ const buttonStyle = "p-2 text-xs rounded-1 cursor-pointer tracking-wide";
 </script>
 
 <template>
-  <div class="cards-container h-full w-full">
-    <div class="flex flex-col gap-6 items-center px-6" v-if="currentCard">
-      <div
-        class="card flex-col gap-4 flex-center min-w-80px min-h-80px overscroll-auto"
-      >
-        <p v-html="getHtml()"></p>
-      </div>
+  <div class="cards-container h-full w-full overflow-hidden p-4">
+    <div class="h-full w-full overflow-auto flex-center">
+      <div class="card-content" v-if="currentCard" v-html="getHtml()" />
     </div>
-    <button
-      v-if="!isRevealCard"
-      @click="onRevealCard"
-      :class="'bg-strong text-white ' + buttonStyle"
-    >
-      Reveal
-    </button>
-    <ul class="flex flex-row gap-4">
+
+    <div class="flex flex-row gap-4">
+      <button
+        v-if="!isRevealCard"
+        @click="onRevealCard"
+        :class="'bg-strong text-white ' + buttonStyle"
+      >
+        Reveal
+      </button>
       <!-- <button
         v-if="isRevealCard"
         @click="onTestCard"
@@ -92,7 +89,7 @@ const buttonStyle = "p-2 text-xs rounded-1 cursor-pointer tracking-wide";
       >
         Next
       </button>
-    </ul>
+    </div>
   </div>
 </template>
 
@@ -104,20 +101,18 @@ div.cards-container {
   grid-template-rows: 1fr 60px;
 }
 
-div.card {
-  overflow: auto;
-  max-height: 400px;
-  max-width: 700px;
-}
 div.card img {
   aspect-ratio: 1;
   object-fit: contain;
   max-height: 200px;
 }
 
-div > * {
-  display: flex;
-  gap: 0.4em;
-  flex-direction: column;
+div.card-content :deep(img) {
+  object-fit: contain;
+}
+
+div.card-content :deep(*) {
+  max-height: 18em !important;
+  line-height: 1.6rem;
 }
 </style>
